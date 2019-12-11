5efe1b153143570e884d481d74ff0222f44f7dc5
hoanghuy226 <hoanghuy226@gmail.com>
2019-12-11 21:10:47 +0700

    done

--- src/index.js
+++ src/index.js
@@ -1,90 +1,18 @@
 require("dotenv").config();
-const fs = require("fs");
-const { promisify } = require("util");
-const readFileAsync = promisify(fs.readFile);
-const { ecc, codec, AccountType } = require("@iceteachain/common");
+const { ecc } = require("@iceteachain/common");
 const Hash = require("ipfs-only-hash");
+const fs = require("fs");
 const http = require("http"),
+  authorize = require("./authorize"),
   formidable = require("formidable"),
-  httpProxy = require("http-proxy"),
-  authorize = require("./authorize");
+  { PassThrough } = require("stream");
 
-let proxy = httpProxy.createProxyServer({});
-let target = process.env.IPFS_HOST;
-console.log("target", target);
+const isAuthorized = (body, headers) => {
+  return true;
+};
 
-// function someAsyncFunc() {
-//   return new Promise(resolve => {
-//     resolve({ data: 1 });
-//   });
-// }
-function formidablePromise(req) {
-  return new Promise((resolve, reject) => {
-    var form = new formidable.IncomingForm();
-    form.parse(req, (err, fields, files) => {
-      if (err) return reject(err);
-      resolve({ fields: fields, files: files });
-    });
-  });
-}
-
-proxy.on("proxyReq", async (proxiedReq, req, res, options) => {
-  try {
-    proxiedReq.on("socket", function(socket) {
-      if (server) {
-        server.emit("proxyReq", proxyReq, req, res, options);
-      }
-    });
-    proxiedReq.proxyWait = new Promise(function(resolve) {
-      setTimeout(() => resolve("done!"), 5000);
-    });
-
-    (proxiedReq.proxyWait || Promise.resolve()).then(data => {
-      console.log("data", data);
-      proxiedReq.setHeader("origin", "");
-      proxiedReq.setHeader("host", "");
-    });
-
-    // const data = await formidablePromise(req);
-    // console.log("data", data);
-    // let promise = new Promise((resolve, reject) => {});
-    // const aaaaa = await promise;
-
-    console.log("done");
-    let token = req.headers.authorization.slice(7); // get token from 'Bearer {token}'
-    token = JSON.parse(token);
-    Object.keys(token.signs).forEach(ipfsHash => {
-      const timeServer = Date.now();
-      const item = token.signs[ipfsHash];
-      const { sign, time } = item;
-      const isLess30Second = Math.floor(timeServer - time) / 1000 < 30;
-      const hash32bytes = ecc.stableHashObject(ipfsHash + time);
-      const isSigned = ecc.verify(hash32bytes, sign, token.pubkey);
-      console.log("isSigned", isSigned, "- isLess30Second: ", isLess30Second);
-    });
-  } catch (error) {
-    console.log("error", error);
-    res.statusCode = 404;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Cannot " + req.method + " " + req.url);
-  }
-
-  // const isSign = ecc.verify(token.);
-  // proxiedReq.setHeader("X-Special-Proxy-Header", "foobar");
-
-  // const data = await formidablePromise(req);
-  // Update header
-  // proxiedReq.setHeader("content-type", "text/plain");
-  // proxiedReq.setHeader("content-length", 0);
-  // proxiedReq.setHeader("origin", "");
-  // proxiedReq.setHeader("host", "");
-  // Write out body changes to the proxyReq stream
-  // proxiedReq.write("");
-  console.log("aaaa");
-});
-
-let server = http.createServer();
-server.on("request", async (req, res) => {
+const httpServer = http.createServer();
+httpServer.on("request", async (req, res) => {
   if (req.method === "OPTIONS") {
     res.setHeader("access-control-allow-origin", "*");
     res.setHeader("access-control-allow-credentials", "true");
@@ -93,16 +21,108 @@ server.on("request", async (req, res) => {
     res.setHeader("access-control-max-age", 86400);
     res.setHeader("Content-Type", "text/plain charset=UTF-8");
     res.setHeader("Content-Type", 0);
-    res.writeHead(204);
+    res.writeHead(204); // no content
     res.end();
   } else {
-    // const data = await formidablePromise(newObj);
-    // const data = await someAsyncFunc();
+    const length = Number(req.headers["content-length"]);
+    if (!length) {
+      // it is an invalid request
+      res.writeHead(400); // bad request
+      res.end();
+      return;
+    }
 
-    proxy.web(req, res, { target: target });
-    console.log("end");
-    // res.end(JSON.stringify(data));
+    const memStream = new PassThrough();
+    req.pipe(memStream);
+
+    const form = new formidable.IncomingForm();
+    const formData = [];
+    form.onPart = part => {
+      let buf = Buffer.alloc(length);
+      let startIndex = 0;
+
+      part.on("data", chunk => {
+        chunk.copy(buf, startIndex);
+        startIndex += chunk.length;
+      });
+      part.on("end", () => {
+        formData.push(buf.slice(0, startIndex));
+        buf = Buffer.alloc(length);
+        startIndex = 0;
+      });
+    };
+
+    form.parse(req, async err => {
+      if (err) {
+        res.writeHead(400); // bad request
+        res.end();
+        return;
+      }
+
+      let token = req.headers.authorization.slice(7); // get token from 'Bearer {token}'
+      const { from, sign, time, pubkeySigner } = JSON.parse(token);
+
+      // const address = ecc.toAddress(from);
+      let isAuthen = false;
+      try {
+        console.log("from", from);
+        isAuthen = await authorize.checkAuthorize(from);
+        console.log("isAuthen", isAuthen);
+        if (!isAuthen) {
+          res.writeHead(401); // unauthorized
+          res.end();
+          return;
+        }
+      } catch (e) {
+        res.writeHead(401); // unauthorized
+        res.end();
+        return;
+      }
+
+      let preHash = [];
+      formData.forEach(buffer => {
+        // console.log("buffer", buffer);
+        preHash.push(Hash.of(buffer));
+      });
+      preHash = await Promise.all(preHash);
+      // console.log("preHash", preHash);
+      let concatHash = "";
+      preHash.forEach(hash => {
+        concatHash = concatHash.concat(hash);
+      });
+      const timeServer = Date.now();
+      const isLess60Second = Math.floor(timeServer - time) / 1000 < 60;
+      const hash32bytes = ecc.stableHashObject(concatHash + time);
+      const isSigned = ecc.verify(hash32bytes, sign, pubkeySigner);
+      console.log("isSigned", isSigned, "- isLess60Second: ", isLess60Second);
+
+      if (isLess60Second && isSigned) {
+        const proxyReq = http.request(
+          process.env.IPFS_HOST + req.url,
+          {
+            method: req.method,
+            headers: { ...req.headers, "content-length": length }
+          },
+          ipfsRes => {
+            console.log("Got response from IPFS");
+            Object.keys(ipfsRes.headers).forEach(key => {
+              const value = ipfsRes.headers[key];
+              res.setHeader(key, value);
+            });
+            res.writeHead(ipfsRes.statusCode); // no content
+            ipfsRes.pipe(res, { end: true });
+          }
+        );
+        //proxyReq.write(body)
+        memStream.pipe(proxyReq, { end: true });
+        //proxyReq.end()
+      } else {
+        res.writeHead(401); // unauthorized
+        res.end();
+        return;
+      }
+    });
   }
 });
 
-server.listen(5050);
+httpServer.listen(5050);
