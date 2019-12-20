325fbb0fec100d6518f725ea1162957eb4f44fc7
Truong Hong Thi <thi1981@gmail.com>
2019-12-20 23:51:18 +0700

    Support alias

--- src/authorize.js
+++ src/authorize.js
@@ -2,13 +2,16 @@ const { IceteaWeb3 } = require("@iceteachain/web3");
 const rpc = process.env.ICETEA_RPC || "https://rpc.icetea.io";
 const tweb3 = new IceteaWeb3(rpc);
 
-// Change the contract after each deployed
-const contract = tweb3.contract(process.env.LOVELOCK_CONTRACT);
+let ct = process.env.LOVELOCK_CONTRACT
+const contract = tweb3.contract(ct);
 
 module.exports = {
-  isAuthorized: function(mainAddress, tokenAddress) {
-      return contract.methods
-        .isAuthorized(mainAddress, tokenAddress, process.env.LOVELOCK_CONTRACT)
-        .call()
+  isAuthorized: async function(mainAddress, tokenAddress) {
+    if (ct.includes('.')) {
+      ct = await tweb3.contract('system.alias').resolve(ct).call()
+    }
+    return contract.methods
+      .isAuthorized(mainAddress, tokenAddress, ct)
+      .call()
   }
 };
