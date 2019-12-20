74aa61a3813a1e45fbd794916e0357ecb09c6eb6
Truong Hong Thi <thi1981@gmail.com>
2019-12-21 01:32:20 +0700

    Pass alias directly to contract

--- src/authorize.js
+++ src/authorize.js
@@ -2,14 +2,11 @@ const { IceteaWeb3 } = require("@iceteachain/web3");
 const rpc = process.env.ICETEA_RPC || "https://rpc.icetea.io";
 const tweb3 = new IceteaWeb3(rpc);
 
-let ct = process.env.LOVELOCK_CONTRACT
+const ct = process.env.LOVELOCK_CONTRACT
 const contract = tweb3.contract(ct);
 
 module.exports = {
   isAuthorized: async function(mainAddress, tokenAddress) {
-    if (ct.includes('.')) {
-      ct = await tweb3.contract('system.alias').methods.resolve(ct).call()
-    }
     return contract.methods
       .isAuthorized(mainAddress, tokenAddress, ct)
       .call()
